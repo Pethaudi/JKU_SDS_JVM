6624f40fef45eb3944c64f0f92251b483e1f7d45
peterhauer <pethaudi@yahoo.de>
2018-06-01 17:27:37 +0200

    created the main entitiy for the table

---
+++ src/entities/Worker_Entities.kt
@@ -0,0 +1,27 @@
+package entities
+
+import java.sql.Date
+import java.sql.Timestamp
+
+data class JKU_SDS_Entry(
+        val id: Number,
+        val degreeOfLatitude: Number,
+        val degreeOfLongitude: Number,
+        val date: Date,
+        val time: Timestamp,
+        val terrain: String,
+        val distanceToWater: Number,
+        val city: String,
+        val continent:String,
+        val weather: String,
+        val temperature: Number,
+        val windvelocity: Number,
+        val winddirection: Number,
+        val airpressure: Number,
+        val sunrise: Timestamp,
+        val sunset: Timestamp,
+        val minAfterSunrise: Number,
+        val minPreSunrise: Number,
+        val gym: Number,
+        val pokestop: Number
+)
