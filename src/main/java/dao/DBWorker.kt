29adb75ed0518141f03724096bf0d74db6062309
peterhauer <pethaudi@yahoo.de>
2018-06-01 20:16:49 +0200

    added jetbrains repo

--- src/main/java/dao/DBWorker.kt
+++ src/main/java/dao/DBWorker.kt
@@ -1,7 +1,12 @@
 package dao
 
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.SchemaUtils.drop
 
 class DBWorker{
-
+    fun start(){
+        val db = Database.connect("", Driver = "")
+    }
 }
