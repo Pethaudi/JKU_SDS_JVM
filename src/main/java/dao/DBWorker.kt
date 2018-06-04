9514f7f0828755ac376881196a354157b37ede16
peterhauer <pethaudi@yahoo.de>
2018-06-04 11:38:46 +0200

    db is now ready

--- src/main/java/dao/DBWorker.kt
+++ src/main/java/dao/DBWorker.kt
@@ -1,15 +1,17 @@
 package dao
 
 import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
 import javax.persistence.Persistence
 
 
 class DBWorker{
     companion object {
-        val factory = Persistence.createEntityManagerFactory("myPU")
+        val factory: EntityManagerFactory
         val db: EntityManager;
 
         init {
+            factory= Persistence.createEntityManagerFactory("myPU")
             db = factory.createEntityManager()
         }
 
