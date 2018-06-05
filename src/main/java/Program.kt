cb02005a16f3184ab92465f03551f044971f20d6
peterhauer <pethaudi@yahoo.de>
2018-06-05 11:45:40 +0200

    added JsonWorker (for writing to the file)

--- src/main/java/Program.kt
+++ src/main/java/Program.kt
@@ -2,6 +2,7 @@ import DataForeman.AppearanceData
 import dao.CsvWorker
 import dao.DbWorker
 import digger.BasicSizes
+import entities.AppearanceContinent
 
 fun main(args: Array<String>){
     //AppearanceData().start()
@@ -9,5 +10,10 @@ fun main(args: Array<String>){
     val worker = CsvWorker()
     //println("millisec: ${worker.measureReadInTime()}")
     //BasicSizes.getAllContinents().forEach { println(it) }
-    BasicSizes.getAppearancesPerContinent().forEach { println("${it.continent} ${it.calcPercentage(BasicSizes.appearances.count())}%") }
+    /*BasicSizes.getAppearancesPerContinent()
+            .forEach {
+                println("${it.continent} ${it.calcPercentage(BasicSizes.appearances.count())}%")
+            }*/
+
+    val json = BasicSizes.getAppearancesPerContinent().map { AppearanceContinent.toJson(it) }
 }
---
+++ src/main/java/dao/JsonWorker.kt
@@ -0,0 +1,32 @@
+package dao
+
+import java.io.File
+import javax.json.Json
+import javax.json.JsonArray
+import javax.json.JsonObject
+
+class JsonWorker {
+    fun writeToFile(name: String, arr: JsonArray){
+        val file = createFile(name)
+        file.writeText(arr.toString())
+    }
+
+    fun writeToFile(name: String, obj: JsonObject){
+        val file = createFile(name)
+        file.writeText(obj.toString())
+    }
+
+    fun writeToFile(name: String, arr: List<JsonObject>){
+        val builder = Json.createArrayBuilder()
+        arr.forEach { builder.add(it) }
+        writeToFile(name, builder.build())
+    }
+
+    private fun createFile(name: String): File {
+        val file = File("results/$name.json")
+        if(file.exists())
+            file.delete()
+        file.createNewFile()
+        return file
+    }
+}
