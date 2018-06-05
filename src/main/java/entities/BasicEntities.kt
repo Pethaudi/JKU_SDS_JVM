b6e5adf4171dcc9be4aeb2428991dcb66cfaa930
peterhauer <pethaudi@yahoo.de>
2018-06-05 11:32:42 +0200

    added BasisEntities and BasicSizes

--- src/main/java/entities/BasicEntities.kt
+++ src/main/java/entities/BasicEntities.kt
@@ -1,5 +1,20 @@
 package entities
 
+import javax.json.Json
+import javax.json.JsonObject
+import javax.json.JsonObjectBuilder
+
+/*
+every object should import this interface
+ */
+interface IJson{
+    fun toJson(obj: Any): JsonObject
+    fun toObject(json: JsonObject): Any
+}
+
+/*
+this needed for some calcs (mostly percentage)
+ */
 open class Counter(
         var counter: Int = 0
 ){
@@ -10,6 +25,37 @@ open class Counter(
     }
 }
 
-data class AppearanceContinent(
-        val continent: String
-) : Counter()
+/*
+self-explaining
+ */
+class AppearanceContinent(
+        val continent: String,
+        counter: Int = 0
+) : Counter(counter) {
+
+    /*
+    cause static
+     */
+    companion object : IJson {
+
+        override fun toJson(obj: Any): JsonObject {
+
+            if(obj is AppearanceContinent) {
+                val builder = Json.createObjectBuilder()
+
+                return builder.add("continent", obj.continent)
+                        .add("counter", obj.counter)
+                        .build()
+            }
+            throw IllegalArgumentException()
+        }
+
+        override fun toObject(json: JsonObject): Any {
+            try{
+                return AppearanceContinent(json["continent"]!!.toString(), json["counter"]!!.toString().toInt())
+            } catch(ex: Exception){
+                throw IllegalArgumentException()
+            }
+        }
+    }
+}
